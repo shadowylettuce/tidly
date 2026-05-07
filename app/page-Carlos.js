6e55f7883fef5820d95be7ec1715d41c26057970
Carlos <cgranillo2005@gmail.com>
2026-05-07 11:29:37 -0700

    remove duplicate Carlos Files

--- app/page-Carlos.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-950">
-      <h1 className="text-white text-5xl font-bold">Tidly</h1>
-      <p className="text-gray-400 mt-4 text-lg">Business management for cleaning companies</p>
-    </main>
-  )
-}
